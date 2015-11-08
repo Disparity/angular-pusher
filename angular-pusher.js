d2f8b4e99890ee9e06966de10dc64b0b6c6e75d1
Disparity <disparity-github@yandex.ru>
2015-11-08 18:30:13 +0600

    Split pusher provider and pusher factory

--- angular-pusher.js
+++ angular-pusher.js
@@ -4,14 +4,14 @@
 angular.module('angular-pusher', [])
     .provider('pusher', [function () {
         var self = this;
-        var config = {key: '', channels: [], options: {debug: false, debounce: 50}, aliases: {}};
+        var config = {key: '', channels: [], options: {debug: false, debounce: 50, aliases: {}}};
         self.subscribe = angular.bind(config.channels, config.channels.push);
         self.alias = function (channel, alias) {
             if (angular.isObject(channel)) {
-                angular.extend(config.aliases, channel);
+                angular.extend(config.options.aliases, channel);
                 return;
             }
-            config.aliases[channel] = alias;
+            config.options.aliases[channel] = alias;
         };
         self.option = function (name, value) {
             if (angular.isObject(name)) {
@@ -28,9 +28,8 @@ angular.module('angular-pusher', [])
         self.setApiKey = function (apiKey) {
             config.key = apiKey || config.key;
         };
-        this.$get = ['$window', function ($window) {
-            var pusher = new $window.Pusher(config.key, config.options);
-            pusher.channelAliases = config.aliases;
+        this.$get = ['$pusher', function ($pusher) {
+            var pusher = $pusher(config.key, config.options);
             for (var i in config.channels) {
                 pusher.subscribe(config.channels[i]);
             }
@@ -43,28 +42,35 @@ angular.module('angular-pusher', [])
             return pusher;
         }];
     }])
-    .run(['pusher', '$rootScope', '$timeout', '$log', function (pusher, $rootScope, $timeout, $log) {
+    .service('$pusher', ['$window', '$rootScope', '$timeout', '$log', function ($window, $rootScope, $timeout, $log) {
         var debouncer = null;
-        var digest = pusher.config.debounce > 0 ? angular.bind($rootScope, $rootScope.$digest) : function () {
-            debouncer && $timeout.cancel(debouncer);
-            debouncer = $timeout(angular.noop, pusher.config.debounce).finally(function () {debouncer = null;});
-        };
+        return function (apiKey, options) {
+            var digest = options.debounce > 0 ? angular.bind($rootScope, $rootScope.$digest) : function () {
+                debouncer && $timeout.cancel(debouncer);
+                debouncer = $timeout(angular.noop, options.debounce).finally(function () {debouncer = null;});
+            };
 
-        pusher.connection.bind_all(function (eventName, data) {
-            pusher.config.debug && $log.debug([eventName, data]);
-            var event = {name: eventName, data: data};
-            switch(eventName) {
-                case 'message':
-                    angular.extend(event, {
-                        name: (data.channel ? (pusher.channelAliases[data.channel] || data.channel) + ':' : '') + data.event,
-                        data: data.data
-                    });
-                    break;
-                default: return;
-            }
-            $rootScope.$broadcast(event.name, event.data);
-            digest();
-        });
+            var pusher = new $window.Pusher(apiKey, options);
+            pusher.connection.bind_all(function (eventName, data) {
+                options.debug && $log.debug([apiKey, eventName, data]);
+            });
+            pusher.connection.bind_all(function (eventName, data) {
+                var event = {name: eventName, data: data, key: apiKey};
+                switch(eventName) {
+                    case 'message':
+                        angular.extend(event, {
+                            name: (data.channel ? (options.aliases[data.channel] || data.channel) + ':' : '') + data.event,
+                            data: data.data
+                        });
+                        break;
+                    default: return;
+                }
+                $rootScope.$broadcast(event.name, event.data);
+                digest();
+            });
+            return pusher;
+        }
     }])
+    .run(['pusher', angular.noop])
 ;
 })(window.angular);
